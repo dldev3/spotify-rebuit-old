51e3c5d049082d6685cd5a750c247850149c381d
dlshnkLK <dilshankaliyanage@yahoo.com>
2022-01-03 23:09:58 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Spotify from "./components/spotify.vue";
+
+</script>
+
+<template>
+ <Spotify />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/spotify.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col h-screen">
+        <div class="bg-blue-500 flex-1 flex">
+
+        </div> 
+    </div>
+</template>
